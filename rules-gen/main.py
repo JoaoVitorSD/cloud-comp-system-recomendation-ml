1cd4cb577e8aa0fd26b898c4699c9bd86d806dfa
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2023-11-14 16:43:29 -0300

    rule gen fix

--- rules-gen/main.py
+++ rules-gen/main.py
@@ -1,21 +1,24 @@
 from fpgrowth_py import fpgrowth
 import pandas as pd
 
-df = pd.read_csv("test.csv")
-grouped_df = df.head(10).groupby('pid')
-grouped_df_dics =  grouped_df.apply(lambda group: group.to_dict(orient='records')).to_dict()
-freqItemSet, rules = fpgrowth(grouped_df["track_uri"].apply(list).values.tolist(), minSupRatio=0.06, minConf=0.01)
-
-def filter_playlist(playlist, rules):
-    for rule in rules:
-        if any({track['track_uri']} == rule for track in playlist):
-            return False
+df = pd.read_csv("2023_spotify_ds1.csv")
+grouped_df = df.groupby('pid')
+grouped_df_dics =  grouped_df["track_uri"].apply(list).to_dict()
+freqItemSet, rules = fpgrowth(grouped_df["track_uri"].apply(list).values.tolist(), minSupRatio=0.07, minConf=0.2)
+# print(rules)
+def filter_playlist(tracks, rules):
+    if not(set(tracks).intersection(rules)):
+        # print({track['track_uri']})
+        return False
     return True
 
-def find_matchs (rule):
+def find_matchs(rule):
     matchs = []
+    # Adicionando a música da regra nas músicas que irão filtras as músicas compatíveis
+    musics = rule[0]
+    musics.update(rule[1])
     for pid, tracks in grouped_df_dics.items():
-        if filter_playlist(tracks, rule):
+        if filter_playlist(tracks, musics):
             matchs.append(pid)
     return { "playlists":matchs, "rule": [rule[0], rule[1]], "percentage": rule[2]}
     
